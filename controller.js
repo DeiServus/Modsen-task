b2829866911346510e366c47fb05c68ee1ca515c
DeiServus <kokokokukareku07@gmail.com>
2024-03-28 00:51:45 +0300

    adding controller.js, router.js, service.js and so on

---
+++ controller.js
@@ -0,0 +1,7 @@
+const meetupService = require('./service');
+
+class MeetupController {
+
+}
+
+module.exports = new MeetupController();
---
+++ router.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const meetupController = require('./controller');
+
+const router = express.Router();
