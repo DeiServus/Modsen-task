f5c302ad857a0d1866b12b74e6522af1e67c80e9
DeiServus <kokokokukareku07@gmail.com>
2024-03-28 12:26:04 +0300

    update service.js and schema.prisma

--- service.js
+++ service.js
@@ -2,7 +2,46 @@ const {PrismaClient} = require('@prisma/client');
 const prisma = new PrismaClient();
 
 class MeetupService {
+    async getMeetups() {
+        const meetups = await prisma.meetups.findMany();
+        return {meetups: meetups};
+    }
 
+    async getMeetupById(id) {
+        const meetup =  await prisma.meetups.findFirst({
+            where: {
+                id: +id
+            }
+        });
+        return {meetup: meetup};
+    }
+
+    async postMeetup(name, description, tags, time_location) {
+        const meetup = await prisma.meetups.create({
+            data: {
+                name: name,
+                description: description,
+                tags: tags,
+                time_location: time_location
+            }
+        });
+        return {meetup: meetup};
+    }
+
+    async putMeetup(id, name, description, tags, time_location) {
+        const meetup = await prisma.meetups.update({
+            where: {
+                id: id
+            },
+            data: {
+                name: name,
+                description: description,
+                tags: tags,
+                time_location: time_location
+            }
+        });
+        return {meetup: meetup};
+    }
 }
 
 module.exports = new MeetupService();
