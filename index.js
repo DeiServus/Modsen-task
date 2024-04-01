544cc64931e3301c05c38651824b4bbbab167324
DeiServus <kokokokukareku07@gmail.com>
2024-04-01 14:23:24 +0300

    swagger update

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const http = require('http');
+const fs = require('fs');
 const router = require("./router");
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
+const swaggerFile = fs.readFileSync('./swagger.json', 'utf8');
+const swaggerDocument = JSON.parse(swaggerFile);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api', router);
 app.use((err, req, res, next) => {
     return res.status(400).json({message: err.message});
